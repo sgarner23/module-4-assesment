40227daa594bf1fc0bf68c4ec62386f28d72fc48
Stephen Garner <stephenallen.garner@gmail.com>
2021-09-10 17:10:55 -0600

    Extra credit finished

--- process.py
+++ process.py
@@ -5,9 +5,20 @@ def sales_reports(log_file): #delcaring a function called sales_report and requi
 
     for line in log_file: #looping through each line in the .txt file 
         line = line.rstrip() #removing all the whitespace from the line
-        day = line[0:3] #finding the day Tuesday. Looking at the first word in the line that is 3 characters long 
+        day = line[0:3] #finding the day Tuesday. Looking at the first word in the line that is 3 characters long then slicing before the third position 
         if day == "Mon": #looking for if the day is Tuesday and if it is then.... (switched to Mon for part 2)
             print(line) #printing out all deliveries from Tuesday based on the logic above 
 
 
 sales_reports(log_file) #calling the function and passing in our log_file variable which pulles from the um-server-01.text file. 
+
+#extra credit:
+
+def large_deliveries(log_file):
+    for line in log_file:
+        line = line.rstrip()
+        melon = int(line[16:18])
+        if melon > 10:
+            print(line)
+
+large_deliveries(log_file)
--- sql-skills.sql
+++ sql-skills.sql
@@ -112,3 +112,72 @@ select a.title, ar.name
 from album a 
 join artist ar
 on a.artist_id = ar.artist_id;
+
+
+
+-----------extra credit--------------------------
+select * 
+from artist 
+order by name desc
+limit 10;
+
+
+---------------------------------------------------
+
+select * 
+from artist 
+where name like 'Black%';
+
+---------------------------------------------------
+select * 
+from artist 
+where name like '%Black%';
+
+
+--------------------------------------------------
+
+select min(birth_date)
+from employee;
+
+
+--------------------------------------------------
+
+select max(birth_date)
+from employee;
+
+--------------------------------------------------
+
+select count(*) 
+from invoice 
+where billing_state in ('CA', 'TX', 'AZ');
+
+--------------------------------------------------
+
+select pt.track_id
+from playlist_track pt 
+join playlist p
+on p.playlist_id = pt.playlist_id
+where name = 'Music';
+
+-------------------------------------------------
+select t.name 
+from track t 
+join playlist_track pt 
+on pt.track_id = t.track_id
+where playlist_id = 5;
+
+--------------------------------------------------
+select t.name, p.name
+from track t
+join playlist_track pt
+on t.track_id = pt.track_id
+join playlist p 
+on p.playlist_id = pt.playlist_id;
+
+--------------------------------------------------
+
+select t.name, a.title, g.name 
+from track t 
+join album a on a.album_id = t.album_id
+join genre g on g.genre_id = t.genre_id
+where g.name = 'Alternative & Punk';
