08682018108cf01dcdda1aed1274379b3607a61a
Stephen Garner <stephenallen.garner@gmail.com>
2021-09-10 15:39:59 -0600

    Finished Python

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #opens the .txt file so it can comb through it and saving it into a variable called log_file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #delcaring a function called sales_report and requiring a parameter.  
+
+    for line in log_file: #looping through each line in the .txt file 
+        line = line.rstrip() #removing all the whitespace from the line
+        day = line[0:3] #finding the day Tuesday. Looking at the first word in the line that is 3 characters long 
+        if day == "Mon": #looking for if the day is Tuesday and if it is then.... (switched to Mon for part 2)
+            print(line) #printing out all deliveries from Tuesday based on the logic above 
 
 
-sales_reports(log_file)
+sales_reports(log_file) #calling the function and passing in our log_file variable which pulles from the um-server-01.text file. 
